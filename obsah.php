9097720b2e94b903ec93d6be50614591a3c999ed
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 20:33:39 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,13 +1,14 @@
-
+<?php
+$content1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php');
+file_put_contents('hlavicka.php', $content1, FILE_APPEND);
+?>
 
 <?php
+$content2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/stred.php');
+file_put_contents('stred.php', $content2, FILE_APPEND);
+?>
 
-    $string = file_get_contents("https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php");
-
-    if($string === FALSE) {
-         echo "Could not read the file.";
-    } else {
-         echo $string;
-    }
-
+<?php
+$content3 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.php');
+file_put_contents('paticka.php', $content3, FILE_APPEND);
 ?>
