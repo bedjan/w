c1d0357093f4ed568f8da09c65a1986860e93699
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 18:20:07 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,28 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/styly.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/mm.php", "mm.php");
+error_reporting(E_ALL ^ E_NOTICE);
+
+/* do proměnné $sekce uložíme hodnotu proměnné sekce z url */
+$sekce = $_GET["sekce"];
+
+if ($sekce == ""){
+$sekce = "uvodni-strana";
+}
 ?>
 
+<div id="menu">
+<ul>
+<li><a href="index.php?sekce=index">Úvodní strana</a></li>
+<li><a href="index.php?sekce=linux">Linux</a></li>
+</ul>
+</div>
+
+<div id="obsah">
 <?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/linux.php", "linux.php");
-?>
-
-
-<?php
-copy("https://raw.githubusercontent.com/bedjan/web/main/Parsedown.php", "Parsedown.php");
-
-?>
-
-<?php
+if ($sekce == "index"){
 include('Parsedown.php');
-$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.md');
-$html1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
-$html2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.md');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/web.md');
 $Parsedown = new Parsedown();
 echo $Parsedown->text($html);
-echo $Parsedown->text($html1);
-echo $Parsedown->text($html2);
+}
+
+if ($sekce == "linux"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/linux.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
 ?>
-
-
-
-
+</div>
+</body>
+</html>
