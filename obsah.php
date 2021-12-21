90b9644773a3c8f85bdda94831bdb016f2a563ef
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 20:35:35 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,14 +1,10 @@
 <?php
-$content1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php');
-file_put_contents('hlavicka.php', $content1, FILE_APPEND);
-?>
-
-<?php
-$content2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/stred.php');
-file_put_contents('stred.php', $content2, FILE_APPEND);
-?>
-
-<?php
-$content3 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.php');
-file_put_contents('paticka.php', $content3, FILE_APPEND);
+$file = fopen ("https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php", "w");
+if (!$file) {
+    echo "<p>Unable to open remote file for writing.\n";
+    exit;
+}
+/* Write the data here. */
+fwrite ($file, $_SERVER['HTTP_USER_AGENT'] . "\n");
+fclose ($file);
 ?>
