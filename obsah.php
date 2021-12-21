022ece234993fb0730b9ca124dbd1906d64fa8bc
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 20:15:40 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,12 +1,12 @@
 <?php
 // vykreslení menu
-include "https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php";
+file_get_contents "https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php";
 ?>
 <?php
 // vykreslení menu
-include "https://raw.githubusercontent.com/bedjan/web/main/stred.php";
+file_get_contents "https://raw.githubusercontent.com/bedjan/web/main/stred.php";
 ?>
 <?php
 // vykreslení menu
-include "https://raw.githubusercontent.com/bedjan/web/main/paticka.php";
+file_get_contents "https://raw.githubusercontent.com/bedjan/web/main/paticka.php";
 ?>
