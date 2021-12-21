773fedd4372fe2c180b04ae9ffaf0fc1a6f66c32
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 19:10:07 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -2,10 +2,58 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<base target="_blank">
 <title>Osobní stránky</title>
 </head>
 
 <body>
+
+
+
+<p>
+<a href="index.php?sekce=index">Úvodní strana</a>
+<a href="index.php?sekce=multimedia">Multimedia</a>
+<a href="index.php?sekce=linux">Linux</a>
+<a href="index.php?sekce=linux-cli">Linux-cli</a>
+<a href="index.php?sekce=ekologie">Ekologie</a> 
+<a href="index.php?sekce=php">Php</a> 
+<a href="index.php?sekce=germanismy">Germanismy</a> 
+<a href="index.php?sekce=vyziva">Výživa</a> 
+<a href="index.php?sekce=lecba">Léčba</a> 
+<a href="index.php?sekce=polstina">Polština</a>
+</p>
+
+
+<hr>
+
+<form method="get" action="http://www.google.com/search">
+
+<table border="0" align="left" cellpadding="0">
+<tr><td>
+<input type="text"   name="q" size="25"
+maxlength="255" value="" />
+<input type="submit" value="Google" /></td></tr>
+<tr><td align="left" style="font-size:75%">
+
+</td></tr></table>
+
+</form>
+<p><br /></p>
+<form method="get" action="https://duckduckgo.com/?q=%s">
+
+<table border="0" align="left" cellpadding="0">
+<tr><td>
+<input type="text"   name="q" size="25"
+maxlength="255" value="" />
+<input type="submit" value="Duckduckgo" /></td></tr>
+<tr><td align="left" style="font-size:75%">
+
+</td></tr></table>
+
+</form>
+<br />
+
+
 
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
@@ -18,19 +66,21 @@ $sekce = "index";
 }
 ?>
 
+<?php
+$filename = 'Parsedown.php';
+
+if (file_exists($filename)) {
+
+} else {
+copy("https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php", "Parsedown.php");
+//copy("https://raw.githubusercontent.com/bedjan/w/main/Parsedown.php", "Parsedown.php");
+}
+?>
 
 
 
-<div id="menu">
-<ul>
-<li><a href="index.php?sekce=index">Úvodní strana</a></li>
-<li><a href="index.php?sekce=multimedia">Multimedia</a></li>
-<li><a href="index.php?sekce=linux">Linux</a></li>
-<li><a href="index.php?sekce=linux-cli">Linux-cli</a></li>
-</ul>
-</div>
 
-<div id="obsah">
+
 <?php
 if ($sekce == "index"){
 include('Parsedown.php');
@@ -61,8 +111,49 @@ $Parsedown = new Parsedown();
 echo $Parsedown->text($html);
 }
 
+if ($sekce == "ekologie"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/ekologie.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
+if ($sekce == "php"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/php.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
+if ($sekce == "germanismy"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/germanismy.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
+if ($sekce == "vyziva"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/vyziva.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
+if ($sekce == "lecba"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/lecba.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
+if ($sekce == "polstina"){
+include('Parsedown.php');
+$html = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/polstina.md');
+$Parsedown = new Parsedown();
+echo $Parsedown->text($html);
+}
+
 ?>
-</div>
 
 
 
