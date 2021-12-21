f9708659054871d1080882bcdf44d43da9098883
bedjan <29821749+bedjan@users.noreply.github.com>
2021-12-21 20:27:04 +0100

    Update obsah.php

--- obsah.php
+++ obsah.php
@@ -1,14 +1,15 @@
-<?
+<?php
 $content = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/hlavicka.php'); 
 eval($content)
 ?>
 
-<?
+<?php
 $content1 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/stred.php'); 
 eval($content2)
 ?>
 
-<?
+<?php
 $content2 = file_get_contents('https://raw.githubusercontent.com/bedjan/web/main/paticka.php'); 
 eval($content2)
 ?>
+
